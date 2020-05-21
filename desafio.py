5d8b6f453bc51ad15b1fb44576e0f35f83746ada
João Pedro <jpmartins201@live.com>
2020-05-21 18:27:32 -0300

    Adicionando alterações finais
    Criando função para decifrar a mensagem, acesso ao arquivo JSON para decifrar a mensagem e gerar o resumo criptográfico, requisição final com a mensagem decifrada

--- desafio.py
+++ desafio.py
@@ -3,6 +3,19 @@ import string
 import requests
 import hashlib
 
+
+def decrypt(cifrado, chave):
+    mensagem = ''
+    
+    for contador in cifrado:
+        try:
+            i = (alfabeto.index(contador) - chave) % 26
+            mensagem += alfabeto[i]
+        except ValueError:
+            mensagem += contador
+    
+    return mensagem
+
 url_get = 'https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token='
 url_post = 'https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token='
 
@@ -15,3 +28,18 @@ requisicao = requests.get(url_get)
 
 with open('answer.json', 'w') as arquivo:
     json.dump(requisicao.json(), arquivo)
+
+with open('answer.json', 'r+') as arquivo:
+    data = json.load(arquivo)
+    cifrado = data['cifrado'].lower()
+    chave = data['numero_casas']
+    data['decifrado'] = decrypt(cifrado, chave)
+    codificacao = arquivo.encoding
+    data['resumo_criptografico'] = hashlib.sha1(data['decifrado'].encode(codificacao)).hexdigest()
+    arquivo.seek(0)
+    json.dump(data, arquivo)
+    arquivo.truncate()
+
+arquivo_final = {'answer': open('answer.json', 'rb')}
+requisicao = requests.post(url_post, files=arquivo_final)
+print(requisicao.json())
